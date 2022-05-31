7929cbb51f8e13c26c478b1efdc5dda66345efee
unknown <kinekarpallavi21@gmail.com>
2022-05-31 15:33:08 +0530

    [Pallavi]Add:To Add Part Time and calculate its daily wages

--- EmployeewageProgram.php
+++ EmployeewageProgram.php
@@ -1,5 +1,5 @@
 <?php
-/* To dispaly Welcome Message*/
+/* To dispaly Welcome Message to */
 class EmployeeWage{
     //Create non static function
     function welcomeProgram(){
@@ -7,12 +7,21 @@ class EmployeeWage{
     }
       //UC1-Check Employee Attendence by using random function
     function attendance(){    
-    $check=rand(0,1);//To get 0 and 1
+    $check=rand(0,2);//To get 0 and 1
+    //UC3-Add Part time to calulate daily employee wage
+    $is_PartTime=1; //Declare two constant is_PartTime and is_FullTime
+    $is_FullTime=2;
     $empRatePerHrs=20;
-    if($check==1){
-        echo"\n Employee is present";
+    if($check== $is_PartTime){
+        echo"\n Employee is working as part time base";
+        $empHrs=4;
+        $this->computeEmpDailyWage($empRatePerHrs, $empHrs);/*if employee is half day present then Call computeEmpDailyWage funtion 
+        to calculate emloyee daily wage */
+    }
+    elseif($check== $is_FullTime){
+        echo"\n Employee is working as full time base";
         $empHrs=8;
-        $this->computeEmpDailyWage($empRatePerHrs, $empHrs);/*if employee is present then Call computeEmpDailyWage funtion 
+        $this->computeEmpDailyWage($empRatePerHrs, $empHrs);/*if employee is full day present then Call computeEmpDailyWage funtion 
         to calculate emloyee daily wage */
     }
     else{
